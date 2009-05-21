703fae0352d618757d38a406c5e2c2933257ed17
Paul Collier <paching@gmail.com>
2009-05-20 23:53:27 -0400

    Initial project import

---
+++ imprint/settings.py
@@ -0,0 +1,32 @@
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+APPEND_SLASH = True
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.csrf.middleware.CsrfMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+)
+
+ROOT_URLCONF = 'imprint.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.sessions',
+)
+
+from local_settings import *
