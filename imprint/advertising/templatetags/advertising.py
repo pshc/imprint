c1d45b8b309508486331da2ac3cc0654810149bf
Paul Collier <paching@gmail.com>
2009-07-04 15:55:58 -0400

    Added nofollow for ad links

--- imprint/advertising/templatetags/advertising.py
+++ imprint/advertising/templatetags/advertising.py
@@ -16,8 +16,8 @@ def random_image_ad(type):
         return ''
     alt = conditional_escape(ad.caption)
     url = settings.MEDIA_URL + ad.image.url
-    return mark_safe(('<a href="%s" title="%s">' +
-            '<img src="%s" width="%d" height="%d" alt="%s" /></a>')
+    return mark_safe(('<a href="%s" title="%s" rel="nofollow">'
+                      '<img src="%s" width="%d" height="%d" alt="%s" /></a>')
             % (ad.get_redirect_url(), alt, url, ad.image.width,
                 ad.image.height, alt))
 
