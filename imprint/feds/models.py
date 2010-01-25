f0fb5ca18b167417b499d6033a70580e123586a6
Paul Collier <paching@gmail.com>
2010-01-25 16:34:04 -0500

    Feds voting interface

--- imprint/feds/models.py
+++ imprint/feds/models.py
@@ -10,6 +10,10 @@ class Position(models.Model):
 
     def __unicode__(self):
         return self.name
+    
+    @property
+    def acclaimed(self):
+        return self.candidates.count() == 1
 
     class Meta:
         ordering = ['order', 'name']
@@ -29,28 +33,36 @@ class Candidate(models.Model):
     class Meta:
         ordering = ['name']
 
-"""
 class UserAgent(models.Model):
     agent = models.CharField(max_length=500, unique=True)
+
+    def __unicode__(self):
+        return self.agent
 
 class VoteManager(models.Manager):
     def create(self, candidate, user_agent, voted_on=None, **kwargs):
         if isinstance(candidate, basestring):
             candidate = Candidate.objects.get(slug=candidate)
         if isinstance(user_agent, basestring):
-            user_agent = UserAgent.objects.get_or_create(agent=user_agent)
+            user_agent = UserAgent.objects.get_or_create(agent=user_agent)[0]
         if voted_on is None:
             voted_on = datetime.now()
         kwargs.update(locals())
-        del kwargs['self']
+        del kwargs['self'], kwargs['kwargs']
         return super(VoteManager, self).create(**kwargs)
 
 class Vote(models.Model):
     candidate = models.ForeignKey(Candidate, related_name='votes')
     voted_on = models.DateTimeField(default=datetime.now, db_index=True)
     user_agent = models.ForeignKey(UserAgent, null=True)
-    ip = models.IPAddressField()
+    ip = models.IPAddressField(db_index=True)
     objects = VoteManager()
-"""
+
+    class Meta:
+        ordering = ['-voted_on', 'ip']
+
+    def __unicode__(self):
+        return u'Vote for %s on %s from %s' % (self.candidate, self.voted_on,
+                self.ip)
 
 # vi: set sw=4 ts=4 sts=4 tw=79 ai et nocindent:
--- imprint/feds/views.py
+++ imprint/feds/views.py
@@ -3,6 +3,39 @@ from django.http import HttpResponseRedirect
 from issues.models import Issue, Section
 from models import *
 from utils import renders
+import datetime
+
+UW_VOTES_PER_DAY = 10
+UW_VOTE_INTERVAL = 5*60
+EXT_VOTES_PER_DAY = 5
+EXT_VOTE_INTERVAL = 30*60
+
+def can_submit_vote(ip, user_agent, cookies):
+    if ip == '129.97.194.14': # Imprint office?
+        return False
+    # This is unreliable but probably Good Enough(TM)
+    for cookie in cookies:
+        if cookie.startswith('voted'):
+            return False
+
+    # Otherwise, throttle votes with a hard cutoff
+    uw = ip.startswith('129.97.')
+    votes_per_day = UW_VOTES_PER_DAY if uw else EXT_VOTES_PER_DAY
+    interval = UW_VOTE_INTERVAL if uw else EXT_VOTE_INTERVAL
+    today = datetime.date.today()
+    # This would be vulnerable if it didn't record in FRIGGIN' MICROSECONDS
+    votes_today = Vote.objects.filter(ip=ip, voted_on__gte=today
+            ).values_list('voted_on', flat=True).distinct()
+
+    if votes_today.count() >= votes_per_day:
+        return False
+    recently = datetime.datetime.now() - datetime.timedelta(seconds=interval)
+    try:
+        recent_vote = votes_today.get(voted_on__gte=recently)
+        return False
+    except Vote.DoesNotExist:
+        pass
+    return True
 
 @renders('feds/index.html')
 def feds_index(request):
@@ -13,13 +46,24 @@ def feds_index(request):
     except (Issue.DoesNotExist, Section.DoesNotExist, Piece.DoesNotExist):
         pass
     positions = Position.objects.all()
-    if request.method == 'POST':
+    ip = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    voted_on = datetime.datetime.now()
+    can_vote = can_submit_vote(ip, user_agent, request.COOKIES)
+    expires = datetime.date.today() + datetime.timedelta(days=1)
+    if request.method == 'POST' and can_vote:
+        response = HttpResponseRedirect('.')
         for pos in positions:
             try:
                 candidate = pos.candidates.get(slug=request.POST.get(pos.slug))
             except Candidate.DoesNotExist:
                 continue
-        return HttpResponseRedirect('.')
+            vote = Vote.objects.create(candidate=candidate, ip=ip,
+                    user_agent=user_agent, voted_on=voted_on)
+            response.set_cookie(str('voted-' + pos.slug),
+                    value='No, this is not scientific.',
+                    expires=expires.strftime('%a, %d %b %Y %H:%M:%S'))
+        return response
     return locals()
 
 # vi: set sw=4 ts=4 sts=4 tw=79 ai et nocindent:
