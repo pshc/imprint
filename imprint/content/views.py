5f409af462882eff9da96b20716c1497ded206d2
Paul Collier <paching@gmail.com>
2010-02-06 21:27:19 -0500

    Live editability, finally!
    Ordering may be broken.

--- imprint/content/views.py
+++ imprint/content/views.py
@@ -3,8 +3,9 @@ from content.models import *
 import datetime
 from django import forms
 from django.conf import settings
-from django.contrib.admin.models import LogEntry, ADDITION
+from django.contrib.admin.models import LogEntry, ADDITION, CHANGE
 from django.contrib.auth.decorators import permission_required
+from django.db import DatabaseError
 from django.http import Http404, HttpResponse, HttpResponseRedirect, \
         HttpResponseBadRequest
 from django.shortcuts import get_object_or_404, redirect
@@ -15,7 +16,7 @@ import re
 from shutil import move
 from utils import renders, unescape
 
-text_input = lambda: forms.TextInput(attrs={'class': 'vTextField'})
+huge_input = lambda: forms.TextInput(attrs={'class': 'vHugeField'})
 small_input = lambda: forms.TextInput(attrs={'class': 'vSmallField'})
 
 def extract_names_and_positions(name):
@@ -32,33 +33,53 @@ def get_or_create_contributor(name, position):
         except Contributor.DoesNotExist:
             return Contributor.objects.create(name=name, position=position)
 
-def add_artists(image, artists, type, ids):
-    for name, pos in extract_names_and_positions(artists):
-        c = get_or_create_contributor(name, pos)
-        ids.add(c.id)
-        Artist.objects.create(image=image, contributor=c, type=type)
+def convert_unit_back(unit):
+    if unit.type is Copy:
+        copy = unit.copy
+        bylines = ', '.join(unicode(b) for b in copy.bylines)
+        return {'type': 'Copy', 'id': unit.id,
+                'order': unit.order, 'name': 'unit%02d' % unit.order,
+                'title': copy.title, 'body': copy.body,
+                'sources': copy.sources, 'bylines': bylines}
+    elif unit.type is Image:
+        image = unit.image
+        photographers = ', '.join(Artist.objects.filter(image=image, type=
+                PHOTOGRAPHER).values_list('contributor__name', flat=True))
+        graphic_artists = ', '.join(Artist.objects.filter(image=image, type=
+                GRAPHIC_ARTIST).values_list('contributor__name', flat=True))
+        return {'type': 'Image', 'id': unit.id,
+                'order': unit.order, 'name': 'unit%02d' % unit.order,
+                'image': image.image, 'image_url': image.image.url,
+                'cutline': image.cutline, 'courtesy': image.courtesy,
+                'photographers': photographers, 'artists': graphic_artists}
 
-def add_bylines(copy, bylines, ids):
-    for name, pos in extract_names_and_positions(bylines):
-        after_copy, name = name.startswith('-'), name.strip('-').strip()
-        if not name:
-            continue
-        c = get_or_create_contributor(name, pos)
-        ids.add(c.id)
-        Byline.objects.create(copy=copy, contributor=c, position=pos,
-                is_after_copy=after_copy)
+def fill_form_from_piece(piece):
+    headline, slug, deck = piece.headline, piece.slug, piece.deck
+    section = piece.section.id
+    volume, issue = piece.issue.volume, piece.issue.number
+    series = piece.series and piece.series.id
+    is_live, is_featured = piece.is_live, piece.is_featured
+    order, id = piece.order, piece.id
+    form = PieceForm(locals())
+    form.units = map(convert_unit_back, piece.units)
+    return form
 
 class PieceForm(forms.Form):
-    headline = forms.CharField(max_length=100, widget=text_input())
-    slug = forms.SlugField(max_length=100, widget=text_input())
-    deck = forms.CharField(max_length=200, required=False, widget=text_input())
+    headline = forms.CharField(max_length=100, widget=huge_input())
+    slug = forms.SlugField(max_length=100, widget=huge_input())
+    deck = forms.CharField(max_length=200, required=False, widget=huge_input())
     section = forms.ModelChoiceField(Section.objects)
     volume = forms.IntegerField(initial=latest_issue_or(lambda i: i.volume,''),
             widget=small_input())
     issue = forms.IntegerField(initial=latest_issue_or(lambda i: i.number, ''),
             widget=small_input())
     series = forms.ModelChoiceField(Series.objects, required=False)
-    is_featured = forms.BooleanField(required=False, initial=False)
+    is_live = forms.BooleanField(required=False, initial=False,
+            help_text="Public visibility.")
+    is_featured = forms.BooleanField(required=False, initial=False,
+            help_text="Should this appear in the cover preview?")
+    order = forms.IntegerField(required=False, widget=small_input())
+    id = forms.IntegerField(required=False)
 
     def clean_issue(self):
         data = self.cleaned_data
@@ -85,7 +106,7 @@ class PieceForm(forms.Form):
 
     def clean(self):
         data = self.cleaned_data
-        self.units = []
+        self.units = getattr(self, 'units', [])
         order = 0
         errors = []
         self.is_ready = True # Since this is a multi-stage form, can we save?
@@ -106,6 +127,8 @@ class PieceForm(forms.Form):
             order = int(attr('order'))
             max_order = max(order, max_order)
             unit.update({'order': order, 'name': 'unit%02d' % order})
+            if attr('id'):
+                unit['id'] = int(attr('id'))
             self.units.append(unit)
         data = self.cleaned_data
         order = max_order
@@ -177,9 +200,14 @@ class PieceForm(forms.Form):
         deleted_fields = ['type', 'name']
         preserved_fields = ['photographers', 'artists', 'bylines']
         piece = Piece(**self.cleaned_data)
-        piece.is_live = False
-        piece.save()
-        ids = set() if not piece.series \
+        # Overwrite if it already exists
+        if self.cleaned_data.get('id'):
+            piece.contributor_ids = set()
+            piece.save(force_update=True)
+        else:
+            piece.save()
+        # Now deal with units
+        self.contributor_ids = set() if not piece.series \
               else set(piece.series.contributors.values_list('id', flat=True))
         for unit in self.units:
             construct = {'Copy': Copy, 'Image': Image}[unit['type']]
@@ -191,15 +219,61 @@ class PieceForm(forms.Form):
                     preserved[field] = unit[field]
                     del unit[field]
             # OK, finally actually make this unit...
-            unit = construct(piece=piece, **unit)
-            unit.save()
-            add_artists(unit, preserved['photographers'], PHOTOGRAPHER, ids)
-            add_artists(unit, preserved['artists'], GRAPHIC_ARTIST, ids)
-            add_bylines(unit, preserved['bylines'], ids)
+            u = construct(piece=piece, **unit)
+            if 'id' in unit:
+                try:
+                    u.save(force_update=True)
+                except DatabaseError, e:
+                    if str(e) != "Forced update did not affect any rows.":
+                        pass
+            else:
+                u.save()
+            if construct is Image:
+                self.add_artists(u, preserved['photographers'], PHOTOGRAPHER)
+                self.add_artists(u, preserved['artists'], GRAPHIC_ARTIST)
+            elif construct is Copy:
+                self.add_bylines(u, preserved['bylines'])
         # Denormalized list of contributors for the whole piece
-        piece.contributors = ids
+        piece.contributors = self.contributor_ids
         piece.save()
         return piece
+
+    def add_artists(self, image, artists, type):
+        old_artists = dict(((a.contributor.id, a.type), a)
+                for a in image.credits)
+        for name, pos in extract_names_and_positions(artists):
+            c = get_or_create_contributor(name, pos)
+            self.contributor_ids.add(c.id)
+            old = (c.id, type)
+            if old in old_artists:
+                del old_artists[old]
+            else:
+                Artist.objects.create(image=image, contributor=c, type=type)
+        # Clear out any previous credits that no longer exist
+        for ((id, t), artist) in old_artists.iteritems():
+            if t == type:
+                artist.delete()
+
+    def add_bylines(self, copy, bylines):
+        old_bylines = dict((b.contributor.id, b) for b in copy.bylines)
+        for name, pos in extract_names_and_positions(bylines):
+            after_copy, name = name.startswith('-'), name.strip('-').strip()
+            if not name:
+                continue
+            c = get_or_create_contributor(name, pos)
+            self.contributor_ids.add(c.id)
+            old_byline = old_bylines.get(c.id)
+            if old_byline:
+                old_byline.position = pos
+                old_byline.is_after_copy = after_copy
+                old_byline.save()
+                del old_bylines[c.id]
+            else:
+                Byline.objects.create(copy=copy, contributor=c, position=pos,
+                        is_after_copy=after_copy)
+        # Clear out any old bylines that no longer exist
+        for b in old_bylines.itervalues():
+            b.delete()
 
 @permission_required('content.add_piece')
 @renders('content/piece_create.html')
@@ -213,9 +287,35 @@ def piece_create(request):
             LogEntry.objects.log_action(request.user.id,
                     ContentType.objects.get_for_model(piece).id,
                     piece.id, unicode(piece), ADDITION)
-            return HttpResponseRedirect('..')
+            return HttpResponseRedirect('..?issue=%d&volume=%d'
+                    % (piece.issue.number, piece.issue.volume))
     else:
         form = PieceForm()
+        form.is_ready = True
+    uploads = ['upload%d' % i for i in range(5)]
+    return locals()
+
+@permission_required('content.change_piece')
+@renders('content/piece_create.html')
+def piece_change(request, id):
+    title = 'Change piece'
+    root_path = '../../../'
+    modifying = True
+    id = int(id)
+    piece = Piece.objects.get(id=id)
+    if request.method == 'POST':
+        post = request.POST.copy()
+        post['id'] = id
+        form = PieceForm(post, request.FILES)
+        if form.is_valid() and form.is_ready:
+            piece = form.save()
+            LogEntry.objects.log_action(request.user.id,
+                    ContentType.objects.get_for_model(piece).id,
+                    piece.id, unicode(piece), CHANGE)
+            return HttpResponseRedirect('..?issue=%d&volume=%d'
+                    % (piece.issue.number, piece.issue.volume))
+    else:
+        form = fill_form_from_piece(piece)
         form.is_ready = True
     uploads = ['upload%d' % i for i in range(5)]
     return locals()
