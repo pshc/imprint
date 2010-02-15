e31cb6bc1cbb02c88a5165b6f937f040378e448e
Paul Collier <paching@gmail.com>
2010-02-15 01:08:42 -0500

    Tighten up URL slug regexps

--- imprint/archive/urls.py
+++ imprint/archive/urls.py
@@ -4,9 +4,9 @@ urlpatterns = patterns('archive.views',
     url(r'^$', 'archive_index', name='archive-index'),
     url(r'^(\d{4})/$', 'archive_year', name='archive-year'),
     url(r'^(\d{4})/(\w{3})/$', 'archive_month', name='archive-month'),
-    url(r'^(\d{4})/(\w{3})/(\d\d?)/([^/]+)/$', 'pdfissue_detail',
+    url(r'^(\d{4})/(\w{3})/(\d\d?)/([\w-]+)/$', 'pdfissue_detail',
             name='pdfissue-detail'),
-    url(r'^(\d{4})/(\w{3})/(\d\d?)/([^/]+)/page(\d+)/$', 'pdfpage_detail',
+    url(r'^(\d{4})/(\w{3})/(\d\d?)/([\w-]+)/page(\d+)/$', 'pdfpage_detail',
             name='pdfpage-detail'),
 )
 
--- imprint/issues/urls.py
+++ imprint/issues/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('issues.views',
     url(r'^$', 'latest_issue', name='latest-issue'),
     url(r'^(\d{4})/(\w{3})/(\d\d?)/$', 'issue_detail', name='issue-detail'),
-    url(r'^(\d{4})/(\w{3})/(\d\d?)/([^/]+)/$', 'section_detail', name='section-detail'),
+    url(r'^(\d{4})/(\w{3})/(\d\d?)/([\w-]+)/$', 'section_detail', name='section-detail'),
     # Should be last
-    url(r'^(?P<slug>[^/]+)/$', 'area_detail', name='area-detail'),
+    url(r'^(?P<slug>[\w-]+)/$', 'area_detail', name='area-detail'),
 )
 
